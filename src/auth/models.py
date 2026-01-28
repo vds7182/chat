46619272c8155d9813f65984f050525c75205d3d
vds7182 <vds71828@gmail.com>
2026-01-28 14:51:32 +0200

    Fix

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,11 +1,19 @@
 import uuid
-from sqlmodel import SQLModel, Field, Relationship,Column
+from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
 
 class User(SQLModel, table=True):
     __tablename__ = "users"
-    uid: uuid.UUID = Field(sa_column=Column(
-        nullable=False, primary_key=True, default=uuid.uuid4
-    ))
-    username: str
-    email: str
-    password_hash: str
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            UUID(as_uuid=True),
+            primary_key=True,
+            nullable=False,
+            default=uuid.uuid4
+        )
+    )
+    username: str = Field(sa_column=Column(String(50), nullable=False))
+    email: str = Field(sa_column=Column(String(100), nullable=False, unique=True))
+    password_hash: str = Field(sa_column=Column(String(128), nullable=False))
